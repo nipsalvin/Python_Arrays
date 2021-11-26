9bd2bfae3d32d61227264498ed4fdadb15e6f6e6
Sir Mwaniki Alvin IV <68748209+nipsalvin@users.noreply.github.com>
2021-11-26 10:48:13 +0300

    Solution !

---
+++ solution1.py
@@ -0,0 +1,16 @@
+def solution(A, N):
+    for i in range(1, N+1):  # search for 1 to n+1 elements
+        flag = False
+
+        for j in range(N-1):  # iterating through the array
+            if A[j] == i:
+                flag = True
+                break
+        if flag == False:
+            return i
+
+
+A = [-2, 2, -3, 2, 10, 0]
+length = len(A)
+res = solution(A, length)
+print(res)
